6c82e93061000bb334145f0174a278266ed66571
otomad <shuishenme@outlook.com>
2024-04-20 01:27:52 +0800

    Modified an ambiguous text

--- src/Transition.tsx
+++ src/Transition.tsx
@@ -503,7 +503,8 @@ export interface TransitionProps {
 	 * exceeds the waiting time, or the transition is not triggered or not detected, the final style layout exception
 	 * is caused.
 	 *
-	 * It will work properly in function component only, and will not work in class component.
+	 * It will work properly in `<Transition>`/`<CssTransition>` function component only, and will not work in
+	 * `<Transition.Component>`/`<CssTransition.Component>` class component.
 	 */
 	maxTimeout?: TransitionTimeout;
 
