619f91a800c1b7c8e732830e2e7acad762116648
Rantetsu Inori <shuishenme@outlook.com>
2025-02-26 17:49:22 +0800

    feat: add is react instance

---
+++ src/utils/isReactInstance.ts
@@ -0,0 +1,42 @@
+import React from "react";
+import type { JSXElementConstructor, ReactElement, ReactNode } from "react";
+import Transition from "../Transition";
+
+/**
+ * Get React element instance for the React component.
+ * @template TComponent - React functional component.
+ */
+export type ReactElementOf<
+	TComponent extends string | JSXElementConstructor<any>,
+> = ReactElement<
+	TComponent extends React.FC<infer TProps> ? TProps : unknown,
+	TComponent
+>;
+
+type ReactElementType = string | React.JSXElementConstructor<any>;
+
+function isObject(value: unknown): value is object {
+	return value !== null && typeof value === "object";
+}
+
+/**
+ * Determine whether a component is an instance of a certain component type.
+ * @param node - Component instance.
+ * @param element - Component class or function component.
+ * @returns Is its instance?
+ */
+export function isReactInstance<T extends ReactElementType>(
+	node: ReactNode,
+	element: T,
+): node is ReactElementOf<T> {
+	return (
+		React.isValidElement(node) &&
+		isObject(node) &&
+		"type" in node &&
+		node.type === element
+	);
+}
+
+export function isTransitionInstance(node: ReactNode): boolean {
+	return isReactInstance(node, Transition) || isReactInstance(node, Transition.Component);
+}
